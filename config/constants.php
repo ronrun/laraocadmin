cbd7b9486ad06414c125925e0131ab2ec5553e88
YiLong Lee <arhatron@gmail.com>
2020-06-25 20:22:53 +0800

    Laravel 7.x with auth

---
+++ config/constants.php
@@ -0,0 +1,9 @@
+<?php
+
+define('HTTP_SERVER', 'http://www.laravel.local');
+define('HTTP_ADMIN', HTTP_SERVER . '/admin');
+define('HTTP_IMAGES', HTTP_SERVER . '/storage/images');
+define('DEFAULT_ARTICLE_COVER', HTTP_IMAGES . '/no_image.png');
+define('DEFAULT_ARTICLE_THUMB', HTTP_IMAGES . '/no_image.png');
+
+?>
